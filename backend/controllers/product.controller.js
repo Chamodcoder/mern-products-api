4ceb54d8b8df664eb4557837ee5ad9c7cb4d28e2
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-11-07 19:48:02 +0530

    backend developments

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,29 @@
+import Product from '../models/product.model.js';
+
+export const getProducts = async (req,res) =>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true,data: products});
+    } catch (error) {
+        console.log("error in fetching products:",error.message);
+        res.status(500).json({success:false,message:"service error"});
+    }
+}
+
+export const createProducts = async(req,res) =>{
+    const product = req.body; //user will send this data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false,message:"please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+    
+    try{
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct});
+    } catch (error){
+        console.error("Error in Create product:",error.message);
+        res.status(500).json({success:false,message:"Service Error"});
+    }
+}
