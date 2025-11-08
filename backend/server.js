a870c4fa1c0d2d4cad1344f2f05c2aaa5cf6fc83
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-11-08 20:09:07 +0530

    backend modified

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT=process.env.PORT|| 5000;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 
@@ -16,9 +17,9 @@ console.log(process.env.MONGO_URI);
 
 // connectDB();
 
-app.listen(5000,() =>{
+app.listen(PORT,() =>{
     connectDB();
-    console.log('server started at http://localhost:5000');
+    console.log('server started at http://localhost:'+PORT);
 });
 
 
